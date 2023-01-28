886faa8473877b080c68f2a6b88e157526c0209c
Olotu Ude Ebi Nan <ebinan10@gmail.com>
2023-01-28 19:45:02 +0100

    Update to the weather city application

--- src/Route.jsx
+++ src/Route.jsx
@@ -4,14 +4,8 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from '././component/home/Home';
 import Signup from '././component/signup/Signup';
 import Login from '././component/login/Login';
-import Component1 from './component/component1/Component1';
-import Component2 from './component/component2/Component2';
-import Component3 from './component/component3/Component3';
-import Component4 from './component/component4/Component4';
-import Component5 from './component/component5/Component5';
 import Nav from './component/nav/Nav'
 import Protected from './component/protected/ProtectedRoute';
-import Mailer from './component/mailer/Mailer';
 import Footer from './component/footer/Footer';
 import Contact from './component/contact/Contact';
 import About from './component/about/About';
@@ -31,16 +25,11 @@ const Rout = () => {
                 <Route path='login' element={<Login/>}/>
                 <Route path='contact' element={<Contact/>}/>
                 
-                <Route path='component1' element={<Component1/>}/>
-                <Route path='component2' element={<Component2/>}/>
-                <Route path='component3' element={<Component3/>}/>
                 <Route path='about' element={<About/>}/>
-                <Route path='component4' element={<Component4/>}/>
                 <Route path='component5' element={<Protected>
-                  <Component5/>
+                  
                   </Protected>}/>
             </Routes>
-            {/* <Mailer/> */}
         </Router>
         <Footer/>
     </div>
--- src/component/component4/Component4.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Component4 = () => {
-  return (
-    <div>Component4</div>
-  )
-}
-
-export default Component4
